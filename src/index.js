61680b2484b2695c173154ab2889761d479359ae
Ramil Khabibullin <khab1bullin92@yandex.ru>
2020-02-21 21:47:42 +0400

    feat: add reverse-int task solution

--- src/index.js
+++ src/index.js
@@ -4,13 +4,12 @@
 module.exports = function towelSort (matrix) {
     let sortedArr=[];
     if(!matrix) return sortedArr;
-      for(let i=0; i<matrix.length; i++) {
-        if (i % 2==0)  for(let j=0; j<matrix[i].length; j++)
-        sortedArr.push(matrix[i][j]); else
-         for(let j=matrix[i].length-1; j>=0; j--)
-        sortedArr.push(matrix[i][j]);
-      
-        return arr;
-      }
-     
+      for(let i=0; i<matrix.length; i++)
+      if (i % 2==0)  for(let j=0; j<matrix[i].length; j++)
+      sortedArr.push(matrix[i][j]); else
+       for(let j=matrix[i].length-1; j>=0; j--)
+      sortedArr.push(matrix[i][j]);
+    
+      return arr;
 }
+
